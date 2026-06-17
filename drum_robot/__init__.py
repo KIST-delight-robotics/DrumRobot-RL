a6b7f236f7ff0f247c8a2b06c3fabe9e708c0739
innu-lee <taekwonist0210@gmail.com>
2026-06-17 16:50:03 +0900

    feat: del sac

--- drum_robot/__init__.py
+++ drum_robot/__init__.py
@@ -1,3 +1,2 @@
 # 폴더 안의 등록 코드를 불러오도록 강제
 from .tasks.drumrobot import *
-from .tasks.test_sac import *
--- drum_robot/tasks/__init__.py
+++ drum_robot/tasks/__init__.py
@@ -1,3 +1,2 @@
 # Import task modules to register gym tasks on package import
 from . import drumrobot # noqa: F401
-from . import test_sac
